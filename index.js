affbb1dcfe90b01c6ab0fae20d789f84cc1e5198
jay1934 <73715013+jay1934@users.noreply.github.com>
2020-12-14 18:57:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const { Client, Collection } = require('discord.js');
 const client = new Client();
 
 client.commands = new Collection();
-client.cooldowns = new Set();
+client.cooldowns = new Collection([
+  ["commands", new Collection()],
+  ["exp", new Set()],
+]);  
 
 require('./handlers/events.js').init(client);
 require('./handlers/commands.js').init(client);
