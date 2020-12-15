c70af8a53f65a3732624ef6e194aa34cc0ac70f1
jay1934 <73715013+jay1934@users.noreply.github.com>
2020-12-14 19:00:03 -0500

    Update message.js

--- events/client/message.js
+++ events/client/message.js
@@ -1,3 +1,4 @@
+const { Collection, MessageEmbed } = require('discord.js');
 const fs = require('fs');
 
 module.exports = (message, client) => {
@@ -9,7 +10,34 @@ module.exports = (message, client) => {
   );
 
   if (!command) {
-    if (client.cooldowns.has(message.author.id)) return;
+    const pings = JSON.parse(fs.readFileSync('./data/pings.json'));
+    pings.forEach((id) => {
+      const user = message.mentions.users.get(id);
+      if (!user) return;
+      if (!message.deleted) message.delete();
+      const embed = new MessageEmbed()
+        .setColor('RED')
+        .setAuthor(user.tag, user.displayAvatarURL())
+        .setTitle(`${user.tag} was warned!`)
+        .addField('Warning:', 'Trying to ping someone who has pings disabled')
+        .setFooter(`Automatically warned • ${message.guild.name}`);
+
+      message.channel.send(embed);
+      message.guild.channels.cache
+        .get(require('../../config.json').logChannelID)
+        .send(embed);
+
+      const warns = JSON.parse(fs.readFileSync('./data/warnings.json'));
+      warns[message.member.id] = [
+        ...(warns[message.member.id] || []),
+        {
+          exec: message.client.user.id,
+          warning: 'Trying to ping someone who has pings disabled',
+        },
+      ];
+      fs.writeFileSync('./data/warnings.json', JSON.stringify(warns, '', 2));
+    });
+    if (client.cooldowns.get('exp').has(message.author.id)) return;
     const exp = JSON.parse(fs.readFileSync('./data/exp.json'));
     const prev = (exp[message.author.id] || {}).exp || 0;
     const amount =
@@ -33,21 +61,48 @@ module.exports = (message, client) => {
         2
       )
     );
-    client.cooldowns.add(message.author.id);
-    return setTimeout(() => client.cooldowns.delete(message.author.id), 60000);
+    client.cooldowns.get('exp').add(message.author.id);
+    return setTimeout(
+      () => client.cooldowns.get('exp').delete(message.author.id),
+      60000
+    );
   }
 
   if (!message.content.startsWith('+')) return;
 
   const permissions = Object.entries(
     require('../../config.json').permissions
-  ).find(([name]) => new RegExp(`^${command.match}$`).test(name))[1];
+  ).find(([name]) => new RegExp(`^${command.match}$`).test(name))?.[1] ?? require('../../config.json').permissions.ping;
 
   if (
     permissions.length &&
     !permissions.some((id) => message.member.roles.cache.has(id))
   )
     return message.channel.send(`:x: Insufficient permissions!`);
+
+  if (command.cooldown) {
+    if (!client.cooldowns.get('commands').has(command.match))
+      client.cooldowns.get('commands').set(command.match, new Collection());
+
+    const now = Date.now();
+    const timestamps = client.cooldowns.get('commands').get(command.match);
+    const cooldown = command.cooldown * 1000;
+
+    if (timestamps.has(message.author.id)) {
+      const expirationTime = timestamps.get(message.author.id) + cooldown;
+
+      if (now < expirationTime) {
+        const timeLeft = (expirationTime - now) / 1000;
+        return message.channel.send(
+          `:x: Please wait ${timeLeft.toFixed(
+            1
+          )} more second(s) before reusing that command.`
+        );
+      }
+    }
+    timestamps.set(message.author.id, now);
+    setTimeout(() => timestamps.delete(message.author.id), cooldown);
+  }
 
   try {
     command.execute(message, args);
