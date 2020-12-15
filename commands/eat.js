0ed866dae84b1861d0be3a951c179bad0c9add1e
jay1934 <73715013+jay1934@users.noreply.github.com>
2020-12-14 19:41:39 -0500

    Update eat.js

--- commands/eat.js
+++ commands/eat.js
@@ -2,19 +2,27 @@ const fs = require('fs');
 
 module.exports = {
   match: 'eat',
+  cooldown: 10,
   execute(message) {
-    message.channel.send(`You ate a cookie! :cookie:`);
     const cookies = JSON.parse(fs.readFileSync('./data/cookies.json'));
+    const items =
+      JSON.parse(fs.readFileSync('./data/items.json'))[message.author.id] || {};
+    let amount = 1;
+    if (items['Mini Gingy'])
+      amount *= 2 ** items['Mini Gingy'];
+    if (items['Rainbow Gingy'])
+      amount *= 4 ** items['Rainbow Gingy'];
     fs.writeFileSync(
       './data/cookies.json',
       JSON.stringify(
         {
           ...cookies,
-          [message.author.id]: (cookies[message.author.id] || 0) + 1,
+          [message.author.id]: (cookies[message.author.id] || 0) + amount,
         },
         '',
         2
       )
     );
+    message.channel.send(`You ate ${amount} cookie${amount > 1 ? 's' : ''}! ${':cookie:'.repeat(amount)}`);
   },
 };
